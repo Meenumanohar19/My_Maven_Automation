32aa29f385fd55ddfd189494d0e549ec2fd611f2
aniru <aniru@DESKTOP-GGRBDL6>
2023-08-16 23:24:44 +0530

    last commit

--- My_Maven_Automation/src/main/java/abstractSample/AbstractSample.java
+++ My_Maven_Automation/src/main/java/abstractSample/AbstractSample.java
@@ -4,7 +4,11 @@ public abstract class AbstractSample // abstract Class
 {
 	public abstract void print(); //abstract method, no body.
 	
-	public void display()  //non-abstract method
+	/*public AbstractSample()
+	{
+		System.out.println("This is constructor");
+	}*/
+	public /*static*/ void display()  //non-abstract method
 	{
 	System.out.println("This is a non-abstract method");
 	}
